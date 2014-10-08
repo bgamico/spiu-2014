d7702c8cd3bf513b2be634e281f661e1eeb02fb5
bruno.g.amico <bruno.g.amico@2aab2689-c09b-630e-9bcf-1178a8df926c>
2014-10-08 19:58:32 +0000

    Otra actualización al módulo de actividades.
    Se cambia idioma por defecto en los mensajes de errores.
    
    08-10-2014
    
    Bruno

---
+++ trunk/codeigniter/system/language/espanol/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['required']			= "El campo %s es obligatorio.";
+$lang['isset']				= "El campo %s no puede ser vacio.";
+$lang['valid_email']		= "El campo %s debe contener una direcci&oacute;n de email valida.";
+$lang['valid_emails']		= "Todos los campos %s deben contener una direcci&oacute;n de email valida.";
+$lang['valid_url']			= "El campo %s debe contener una URL valida.";
+$lang['valid_ip']			= "El campo %s debe contener una IP valida.";
+$lang['min_length']			= "El campo %s almenos debe tener %s caracteres de longitud.";
+$lang['max_length']			= "El campo %s no debe tener una longitud mayor a %s caracteres.";
+$lang['exact_length']		= "El campo %s debe tener un longitud exacta de %s caracteres.";
+$lang['alpha']				= "El campo %s solo debe contener alphabetical characters.";
+$lang['alpha_numeric']		= "El campo %s solo debe contener caracteres alpha-num&eacute;ricos.";
+$lang['alpha_dash']			= "El campo %s solo debe contener caracteres alpha-num&eacute;ricos, guiones bajos, y barras.";
+$lang['numeric']			= "El campo %s solo debe contener n&uacute;meros.";
+$lang['is_numeric']			= "El campo %s solo debe contener caracteres num&eacute;ricos.";
+$lang['integer']			= "El campo %s debe contener un entero.";
+$lang['regex_match']		= "El campo %s is not in the correct format.";
+$lang['matches']			= "El campo %s does not match El campo %s.";
+$lang['is_unique'] 			= "El campo %s debe contener a unique value.";
+$lang['is_natural']			= "El campo %s debe contener only positive numbers.";
+$lang['is_natural_no_zero']	= "El campo %s debe contener a number greater than zero.";
+$lang['decimal']			= "El campo %s debe contener a decimal number.";
+$lang['less_than']			= "El campo %s debe contener a number less than %s.";
+$lang['greater_than']		= "El campo %s debe contener a number greater than %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
