ff183862747d4085d5a7da3f8440bdf1d240b087
bruno.g.amico <bruno.g.amico@2aab2689-c09b-630e-9bcf-1178a8df926c>
2014-10-08 18:41:17 +0000

    Otras actualizaciones al módulo de actividades. 09-10-2014 Bruno

--- codeigniter/application/views/include/error.php
+++ codeigniter/application/views/include/error.php
@@ -1,6 +1,5 @@
 <div class="alert alert-danger alert-dismissable" style="width: 360px; margin: 0 auto;">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
   <p><strong>Mensaje de Validaci&oacute;n</strong></p> 
-  <p><?= $a ?></p>
   <p>El Usuario o la Clave que has ingresado son incorrectas.</p>
 </div>
