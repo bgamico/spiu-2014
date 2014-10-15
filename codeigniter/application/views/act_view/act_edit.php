08b90628436c966b06f2f6747d497fc2ea41f485
bruno.g.amico <bruno.g.amico@2aab2689-c09b-630e-9bcf-1178a8df926c>
2014-10-15 20:59:15 +0000

    Actualización sobre el módulo de Actividades. Alta, baja y actualización correctas.

--- codeigniter/application/views/act_view/act_edit.php
+++ codeigniter/application/views/act_view/act_edit.php
@@ -1,47 +1,77 @@
 <div class="col-lg-9 offset1">
 <div class="well bs-component">
 
-    <?= form_open('actividadmanage/update', array('class'=>'form-horizontal','id'=>'contact-form')); ?>
+  <?= form_open('actividadmanage/update', array('class'=>'form-horizontal','id'=>'contact-form')); ?>
   <fieldset>
     <legend><?= $titulo ?></legend>
+     
+
+    
+        
+    <?= form_hidden('actividad_id', $registro->actividad_id); ?>
+
+        
 
     <div class="form-group">    
     <div class="control-group">
-        <?= form_label('Id', 'id', array('class'=>'col-lg-3 control-label')); ?>    
-        <span class="uneditable-input"> <?= $registro->actividad_id; ?> </span>
-        <?= form_hidden('actividad_id', $registro->actividad_id); ?>
-    </div>
-    </div>
+        <?= form_label('Nombre*', 'name', array('class'=>'col-lg-3 control-label')); ?>
+        <div class="col-lg-9">        
+        <?= form_input(array('type'=>'text', 'class'=>"form-control", 'name'=>'name', 'id'=>'name', 'placeholder'=>'Nombre de actividad', 'value'=>$registro->name)); ?>
+        <?php echo form_error('name'); ?>
+        </div>
+    </div>  
+	</div>    
     
+    <div class="form-group">    
     <div class="control-group">
-        <?= form_label('Nombre:', 'name', array('class'=>'control-label')); ?>
-        <?= form_input(array('type'=>'text', 'name'=>'name', 'id'=>'name', 'value'=>$registro->name)); ?>
+        <?= form_label('Descripci&oacute;n*', 'descripcion', array('class'=>'col-lg-3 control-label')); ?>
+        <div class="col-lg-9">        
+        <?= form_textarea(array('type'=>'textarea', 'class'=>"form-control", 'name'=>'descripcion', 'id'=>'descripcion', 'placeholder'=>'Descripci&oacute;n', 'value'=>$registro->descripcion, 'cols'=>'4', 'rows'=>'4')); ?>
+        <?php echo form_error('descripcion'); ?>
+        </div> 
     </div>
+    </div>   
 
+    <div class="form-group">    
+    <div class="control-group">   
+        <?= form_label('Fecha*', 'fecha', array('class'=>'col-lg-3 control-label')); ?>
+        <div class="col-lg-9">        
+        <?= form_input(array('type'=>'date', 'class'=>"form-control", 'name'=>'fecha', 'id'=>'fecha', 'value'=>$registro->fecha)); ?>
+        <?php echo form_error('fecha'); ?>
+        </div>
+    </div>        
+    </div>    
+
+    <div class="form-group">    
     <div class="control-group">
-        <?= form_label('Descripci&oacute;n:', 'descripcion', array('class'=>'control-label')); ?>
-        <?= form_textarea(array('type'=>'textarea', 'name'=>'descripcion', 'id'=>'descripcion', 'value'=>$registro->descripcion, 'cols'=>'4', 'rows'=>'4')); ?>
-    </div>
-
+        <?= form_label('Hora*', 'hora', array('class'=>'col-lg-3 control-label')); ?>
+        <div class="col-lg-9">        
+        <?= form_input(array('type'=>'time', 'class'=>"form-control", 'name'=>'hora', 'id'=>'hora', 'value'=>date("H:i", strtotime($registro->hora)))); ?>
+		<?php echo form_error('hora'); ?>        
+        </div>   
+    </div>  
+    </div>    
+    
+    <div class="form-group">    
     <div class="control-group">
-        <?= form_label('Fecha:', 'fecha', array('class'=>'control-label')); ?>
-        <?= form_input(array('type'=>'date', 'name'=>'fecha', 'id'=>'fecha', 'value'=>$registro->fecha)); ?>
+        <?= form_label('Direcci&oacute;n*', 'direccion', array('class'=>'col-lg-3 control-label')); ?>
+        <div class="col-lg-9">        
+        <?= form_input(array('type'=>'text', 'class'=>"form-control", 'name'=>'direccion', 'id'=>'direccion','placeholder'=>'Direcci&oacute;n', 'value'=>$registro->direccion)); ?>
+    	<?php echo form_error('direccion'); ?>        
+        </div>
     </div>
-
-    <div class="control-group">
-        <?= form_label('Hora:', 'hora', array('class'=>'control-label')); ?>
-        <?= form_input(array('type'=>'time', 'name'=>'hora', 'id'=>'hora', 'value'=>date("H:i", strtotime($registro->hora)))); ?>
+    </div>     
+      
+    <p class = "col-lg-3"></p>
+	<p class="col-lg-9" >*<em>campos obligatorios.</em></p>
+    
+    <div class="form-group">
+	    <div class="col-lg-9 col-lg-offset-3">
+	        <?= form_button(array('type'=>'submit', 'content'=>'Aceptar', 'class'=>'btn btn-primary')); ?>  
+	        <?= anchor($urlCancelar, 'Cancelar', array('class'=>'btn btn-default')); ?>
+	    </div>
     </div>
-
-    <div class="control-group">
-        <?= form_label('Direccion:', 'direccion', array('class'=>'control-label')); ?>
-        <?= form_input(array('type'=>'text', 'name'=>'direccion', 'id'=>'direccion', 'value'=>$registro->direccion)); ?>
-    </div>
-
-    <div class="form-actions">
-        <?= form_button(array('type'=>'submit', 'content'=>'Aceptar', 'class'=>'btn btn-primary')); ?>
-        <?= anchor('actividadmanage/search', 'Cancelar', array('class'=>'btn')); ?>
-    </div>
+        
 	</fieldset>  
     <?= form_close(); ?>
 </div>
