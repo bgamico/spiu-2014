4336ba100042b4e6a8583e78a88d1c8db8e919e4
castropnicolas <castropnicolas@2aab2689-c09b-630e-9bcf-1178a8df926c>
2014-11-23 18:58:48 +0000

    Se agrego la ventana(modal) de confirmacion para la eliminación de usuarios. Falta agregar el feedback del usuario eliminado exitosamente.

--- trunk/finaldiciembre/application/views/user_view/user_get.php
+++ trunk/finaldiciembre/application/views/user_view/user_get.php
@@ -2,13 +2,6 @@
 	<div class="bs-docs-section">
 
 		<div class="col-lg-11">
-<!-- 			<div class="page-header"> -->
-<!-- 				<h1> -->
-<!-- 					Usuarios <small> mantenimiento de registros </small> -->
-<!-- 				</h1> -->
-<!-- 			</div> -->
-					<!-- ?= anchor('usuario/add', 'Agregar', array('class'=>'btn btn-primary')); ?-->
-<!-- 			<p></p> -->
 			
 	<div class="bs-example">
     <nav class="navbar navbar-default" role="navigation">
@@ -18,10 +11,6 @@
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
           <form class="navbar-form navbar-right" role="search">
-<!--             <div class="form-group"> -->
-<!--               <input type="text" class="form-control" placeholder="Buscar"> -->
-<!--             </div> -->
-<!--             <button type="submit" class="btn btn-default">Agregar</button> -->
             <?= anchor('usuario/add', 'Agregar', array('class'=>'btn btn-default')); ?>
           </form>
         </div>
@@ -48,7 +37,10 @@
 							<td>
                                 <?= anchor('usuario/view/'.$registro->id, '<i class="glyphicon glyphicon-search"></i>',array('class'=>'view')); ?>
                                 <?= anchor('usuario/edit/'.$registro->id, '<i class="glyphicon glyphicon-pencil"></i>',array('class'=>'view')); ?>
-                                <?= anchor('usuario/delete/'.$registro->id,'<i class="glyphicon glyphicon-remove"></i>',array('onclick' => "return confirm('Se eliminar&aacute; el usuario. &iquest;Est&aacute; seguro&#63')"))?>
+                                <!--?= anchor('usuario/delete/'.$registro->id,'<i class="glyphicon glyphicon-remove"></i>',array('onclick' => "return confirm('Se eliminar&aacute; el usuario. &iquest;Est&aacute; seguro&#63')"))?-->
+                                <a data-href="delete.php?id=23" data-toggle="modal" data-target="#confirm-delete" href="#"><i class="glyphicon glyphicon-remove"></i></a>
+<!-- 								<a data-href="delete.php?id=54" data-toggle="modal" data-target="#confirm-delete" href="#">Delete record #54</a> -->
+                                
 
                             </td>
 						</tr>
@@ -61,8 +53,8 @@
 
 
 	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.css">
-	<script type="text/javascript" language="javascript" src="<?php echo base_url('assets/js/jquery.dataTables.min.js')?>"></script>
-	<script type="text/javascript" language="javascript" src="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/jquery.dataTables.min.js')?>"></script>
+	<script type="text/javascript" src="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
 	
 	<script type="text/javascript" class="init">
 
@@ -70,5 +62,31 @@ $(document).ready(function() {
 	$('#example').dataTable();
 } );
 
+$('#confirm-delete').on('show.bs.modal', function(e) {
+    $(this).find('.danger').attr('href', $(e.relatedTarget).data('href'));
+});
 	</script>
-<style id="bsa_css">.one{position:relative}.one .bsa_it_ad{display:block;padding:15px;border:1px solid #e1e1e1;background:#f9f9f9;font-family:helvetica,arial,sans-serif;line-height:100%;position:relative}.one .bsa_it_ad a{text-decoration:none}.one .bsa_it_ad a:hover{text-decoration:none}.one .bsa_it_ad .bsa_it_t{display:block;font-size:12px;font-weight:bold;color:#212121;line-height:125%;padding:0 0 5px 0}.one .bsa_it_ad .bsa_it_d{display:block;color:#434343;font-size:12px;line-height:135%}.one .bsa_it_ad .bsa_it_i{float:left;margin:0 15px 10px 0}.one .bsa_it_p{display:block;text-align:right;position:absolute;bottom:10px;right:15px}.one .bsa_it_p a{font-size:10px;color:#666;text-decoration:none}.one .bsa_it_ad .bsa_it_p a:hover{font-style:italic}</style><script id="_adpacks_projs" type="text/javascript" src="//cdn.buysellads.com/ac/pro.js"></script><script type="text/javascript" id="_bsaPRO_js" src="//srv.buysellads.com/ads/get/ids/C6SI42Y/?r=1416632400000&amp;segment_C6SI42Y=placement:datatablesnet" async="async"></script></head>
+
+
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title" id="myModalLabel">Confirmar Borrado</h4>
+                </div>
+            
+                <div class="modal-body">
+                    <p>Esta seguro que desea borrar este usuario?</p>
+                </div>
+                
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                    <?= anchor('usuario/delete/'.$registro->id,'Borrar',array('class'=>"btn btn-danger danger"))?>
+<!--                     <a href="#" class="btn btn-danger danger">Borrar</a> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
