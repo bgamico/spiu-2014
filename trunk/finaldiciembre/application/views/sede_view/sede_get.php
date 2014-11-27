0232d343ef53579ae8a3049c873be3e06ba194ed
castropnicolas <castropnicolas@2aab2689-c09b-630e-9bcf-1178a8df926c>
2014-11-27 22:03:16 +0000

    Se actualizo la vista del sidebar

--- trunk/finaldiciembre/application/views/sede_view/sede_get.php
+++ trunk/finaldiciembre/application/views/sede_view/sede_get.php
@@ -34,37 +34,37 @@
       </div>
     </nav>
   </div>
-<div class="row row-offcanvas row-offcanvas-right">
-	<div class="col-xs-12 col-sm-8">
+
+	<div class="col-sm-8">
 	<?=$map['html']?>
 	</div>
-	<div class="col-xs-6 col-sm-2 sidebar-offcanvas" id="sidebar" role="navigation">
-	    	<ul class="nav nav-pills nav-stacked">
-	    	<?php foreach($datos as $marker_sidebar)
+	<hr class="visible-xs" >
+
+<div class="container-fluid" id="main">
+
+  	<div class="col-md-3" id="left">
+    
+      <?php foreach($datos as $marker_sidebar)
 		        {?>
-				  <li role="presentation" onclick="datos_marker(<?=$marker_sidebar->latitud?>,<?=$marker_sidebar->longitud?>,marker_<?=$marker_sidebar->id?>)"><a><?=substr($marker_sidebar->nombre,0)?></a>			  
+				  <div class="panel panel-default">
+			        <div class="panel-heading" >
+			        		<a onclick="datos_marker(<?=$marker_sidebar->latitud?>,<?=$marker_sidebar->longitud?>,marker_<?=$marker_sidebar->id?>)"><?=substr($marker_sidebar->nombre,0)?></a>
+							<a class="pull-right col-md-3" id="delete" data-toggle="modal" data-target="#confirm-delete" href="#"><i class="glyphicon glyphicon-remove"></i></a>
+			        		<?= anchor('sede/edit/'.$marker_sidebar->id, '<i class="glyphicon glyphicon-pencil"></i>',array('class'=>'view pull-right')); ?>
+							 
+					</div>
+			      </div>
+			      		  				  
+				  			  
 				  <?php
 		        }
 		        ?>
-			</ul>
+
+    </div>
     
     </div>
-    <div class="col-xs-6 col-sm-2 sidebar-offcanvas" id="sidebar" role="navigation">
-	    	<ul class="nav nav-pills nav-stacked">
-	    	<?php foreach($datos as $marker_sidebar)
-		        {?>
-		        <li role="presentation"><a></a></li>
-					<?= anchor('sede/edit/'.$marker_sidebar->id, '<i class="glyphicon glyphicon-pencil"></i>',array('class'=>'view')); ?>
-					<!-- ?= anchor('sedemanage/delete/'.$marker_sidebar->id, '<i class="glyphicon glyphicon-remove"></i>',array('class'=>'view', 'id'=>'delete')); ?-->
-					<a id="delete" data-toggle="modal" data-target="#confirm-delete" href="#"><i class="glyphicon glyphicon-remove"></i></a>
-				  <?php
-		        }
-		        ?>
-			</ul>
-    
-    </div>
-	
-</div>
+
+
 </body>
 
 <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
