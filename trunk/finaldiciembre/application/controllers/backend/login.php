df1fb91149307ffb8cb78fcf855f650911c823ae
castropnicolas@gmail.com <castropnicolas@gmail.com@2aab2689-c09b-630e-9bcf-1178a8df926c>
2014-12-16 05:40:11 +0000

    Se cambio el nombre del archivo a account, ahora se llama login.

---
+++ trunk/finaldiciembre/application/controllers/backend/login.php
@@ -0,0 +1,65 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Administrador de cuentas Rbac.
+ *
+ * @author Castro Patricio Nicolas
+*/
+
+class Login extends CI_Controller
+{
+	/**
+	 * pagina de login
+	 * @access public
+	 */
+	function index()
+	{
+		if($this->session->userdata('username')){
+			redirect(base_url('backend/manage'));
+		}
+		else{
+			$this->load->view('backend/include/header');
+			$this->load->view('backend/login_index');
+			$this->load->view('backend/include/footer');
+		}
+	}
+
+	/**
+	 * validar usuario, contrasena y permisos
+	 * @access public
+	 */
+	function validate()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+
+		$md5_password = md5($password);
+
+		$ret = $this->rbac_model->validateUser($username, $md5_password);
+
+		if($ret == 1)
+		{
+
+			redirect(base_url('backend'));
+		}
+		else
+		{
+			$query = $this->Model_Sede->miSede($username);
+			isset( $query[0]->id) AND $this->session->set_userdata('sede',$query[0]->id);
+			$this->session->set_userdata('username',$username);
+			$this->session->set_userdata('rol',$ret['role_id']);
+			redirect(base_url('backend/manage'));
+		}
+
+	}
+
+	/**
+	 * logout:terminar session
+	 * @access public
+	 */
+	function logout()
+	{
+		$this->session->sess_destroy();
+		redirect(base_url('backend'));
+	}
+}
